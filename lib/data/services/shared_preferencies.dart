5a68def7f4e472b566e2c522f8ee613989ea5e97
renatoalves <renatorv@gmaail.com>
2026-07-26 18:58:35 -0300

    MVVM no Flutter #13 SharedPreferences

---
+++ lib/data/services/shared_preferencies.dart
@@ -0,0 +1,31 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../domain/models/user.dart';
+import '../../utils/result.dart';
+
+class SharedPreferencies {
+  static const String _userKey = 'userKey';
+
+  Future<Result<void>> saveUser(User user) async {
+    try {
+      final sharedPreferences = await SharedPreferences.getInstance();
+      await sharedPreferences.setString(_userKey, user.toJson());
+      return Result.ok(null);
+    } on Exception catch (error) {
+      return Result.error(error);
+    }
+  }
+
+  Future<Result<User?>> fetchUser() async {
+    try {
+      final sharedPreferences = await SharedPreferences.getInstance();
+      final result = sharedPreferences.getString(_userKey);
+      if (result == null) {
+        return Result.ok(null);
+      }
+      return Result.ok(User.fromJson(result));
+    } on Exception catch (error) {
+      return Result.error(error);
+    }
+  }
+}
---
+++ lib/domain/models/user.dart
@@ -0,0 +1,59 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class User {
+  final String accessToken;
+  final String refreshToken;
+  final int id;
+  final String username;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String gender;
+  final String image;
+
+  const User({
+    required this.accessToken,
+    required this.refreshToken,
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.gender,
+    required this.image,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'accessToken': accessToken,
+      'refreshToken': refreshToken,
+      'id': id,
+      'username': username,
+      'email': email,
+      'firstName': firstName,
+      'lastName': lastName,
+      'gender': gender,
+      'image': image,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      accessToken: map['accessToken'] as String,
+      refreshToken: map['refreshToken'] as String,
+      id: map['id'] as int,
+      username: map['username'] as String,
+      email: map['email'] as String,
+      firstName: map['firstName'] as String,
+      lastName: map['lastName'] as String,
+      gender: map['gender'] as String,
+      image: map['image'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) =>
+      User.fromMap(json.decode(source) as Map<String, dynamic>);
+}
